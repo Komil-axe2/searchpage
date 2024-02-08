f7e5a70d872ccd613ada4327a0c54af42848fd90
Komil-axiy2 <Komil7024868@gmail.com>
2024-02-08 07:03:21 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+let header = document.querySelector('header')
+
+header.innerHTML = `
+<div class="header-content">
+    <div class="meadia-header">
+        <div class="logo-left">
+            <a href="./index.html">logo</a>
+        </div>
+    </div>
+    <div class="right-header">
+        <a href="logo">Search</a>
+        <a href="./store.html">Store</a>
+        <a href="logo">Menu</a>
+    </div>
+</div>
+<div class="header-link">
+    <a href="./index.html">Home</a>
+    <a href="./store.html">Store</a>
+    <a href="logo">logo</a>
+    <a href="logo">logo</a>
+    <a href="logo">logo</a>
+    <a href="logo">logo</a>
+    <a href="logo">logo</a>
+    <a href="logo">logo</a>
+    <a href="logo">logo</a>
+    <a href="logo">logo</a>
+    <a href="logo">logo</a>
+    <a href="logo">logo</a>
+    <a href="logo">logo</a>
+    <a href="logo">logo</a>
+    <a href="logo">logo</a>
+</div>
+`
